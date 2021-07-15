85c29b6870ce1191b7f042ac62301db635a8320b
mukesha8689 <58267808+mukesha8689@users.noreply.github.com>
2021-07-16 02:43:26 +0530

    first commit

---
+++ Add.java
@@ -0,0 +1,12 @@
+
+
+public class Add{
+
+    public static void main(String[] args)
+    {
+        int a=5;
+        int b=2;
+        System.out.println(a+b);
+    }
+
+}
---
+++ Difference.java
@@ -0,0 +1,13 @@
+
+
+
+public class Difference{
+
+    public static void main(String[] args)
+    {
+        int a=5;
+        int b=2;
+        System.out.println(a-b);
+    }
+
+}
